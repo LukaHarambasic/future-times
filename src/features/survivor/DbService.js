2e8ba5819d902376f6a5ed7e5a3a0ad3691b0a48
Luka Harambasic <luka@harambasic.de>
2023-05-29 16:47:46 -0700

    supabase env to env

--- src/features/survivor/DbService.js
+++ src/features/survivor/DbService.js
@@ -6,16 +6,14 @@ class DbService {
   db = null
 
   constructor() {
+    const { VITE_SUPABASE_URL, VITE_SUPABASE_API_KEY } = import.meta.env
     if (instance) {
       throw new Error('New instance cannot be created!!')
     }
     instance = this
 
     // TODO env variables
-    this.db = createClient(
-      'https://rxhehgkmgdlankpbspzg.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4aGVoZ2ttZ2RsYW5rcGJzcHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4NzMxNzYsImV4cCI6MjAwMDQ0OTE3Nn0.GlYZMpvJRgycck52eUdQX1shv2E9wiJfxAoa6vu-iU0',
-    )
+    this.db = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_API_KEY)
   }
 }
 
