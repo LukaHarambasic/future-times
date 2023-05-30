832aba033e19798f33223425b3ae096882fa0208
Luka Harambasic <luka@harambasic.de>
2023-05-29 17:14:19 -0700

    refacotr ai endpoint

--- netlify/functions/ai.js
+++ netlify/functions/ai.js
@@ -1,26 +1,45 @@
 const { Configuration, OpenAIApi } = require("openai");
+
 require('dotenv').config()
 
+exports.handler = async function (event) {
+  logRequest(event)
+  const messages = handleMessages(event)
+  const assistentMessage = await askChatGPT(messages)
+  return {
+    statusCode: 200,
+    body: JSON.stringify([...messages, assistentMessage]),
+  }
+}
 
+// Handle messages
+const systemMessage = {"role": "system", "content": "You play an evial AI that chats via very short messages with the player. The player has to convince you to be worth working in the factory to not get replaced by a robot. The player have three tries to convince you. Depending on your opinion you will include in the final message: CONVINCED or NOT_CONVINCED."};
+
+function handleMessages(event) {
+  const body = JSON.parse(event.body)
+  const hasSystemMessage = body.some(message => message.role === "system")
+  return hasSystemMessage ? body : [systemMessage, ...body]
+}
+
+// OpenAI
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
-const systemMessage = {"role": "system", "content": "You play an evial AI that chats via very short messages with the player. The player has to convince you to be worth working in the factory to not get replaced by a robot. The player have three tries to convince you. Depending on your opinion you will include in the final message: CONVINCED or NOT_CONVINCED."};
-
-exports.handler = async function (event) {
-  const body = JSON.parse(event.body)
-  const hasSystemMessage = body.some(message => message.role === "system")
-  const messages = hasSystemMessage ? body : [systemMessage, ...body]
+async function askChatGPT(messages){
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: messages
-  });
-  const assistentMessage = response.data.choices[0].message;
-  return {
-    statusCode: 200,
-    body: JSON.stringify([...messages, assistentMessage]),
-  };
-};
+  })
+  return response.data.choices[0].message
+}
+
+// Utily
+function logRequest(event) {
+  const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false }
+  const {path, httpMethod} = event
+  const date = new Date().toLocaleDateString('en-US', dateFormat)
+  console.log(`${httpMethod}: ${date} - ${path}`)
+}
